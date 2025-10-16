fee1a67cd41c11c33afae77b6c0e91b8954969da
qsc20001102 <qsc20001102@163.com>
2025-10-16 15:20:37 +0800

    12

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import tkinter as tk
+from core.ui_main import MainUI
+from core.logger_config import setup_logger
+
+if __name__ == "__main__":
+    setup_logger()
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    #print(base_dir)
+    root = tk.Tk()
+    app = MainUI(root, base_dir)
+    root.mainloop()
